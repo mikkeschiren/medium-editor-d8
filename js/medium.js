953ae24382f3b908918b4c7adb1d036708b328eb
mikkeschiren <mikke.schiren@wunderkraut.com>
2015-02-12 15:50:05 +0100

    added some local css

--- js/medium.js
+++ js/medium.js
@@ -19,7 +19,6 @@ if (Drupal.editors) Drupal.editors.medium = {
   $field.parent().parent().parent().find('label').hide();
   $field.hide();
   var editor = new MediumEditor('.editable-wrapper');
-  console.log(Drupal.editors.medium);
   },
   detach: function (element, format, trigger) {
     return MediumEditor.detach(element);
@@ -27,14 +26,4 @@ if (Drupal.editors) Drupal.editors.medium = {
   onChange: function (element, callback) {
   }
 };
-// When the form is submitted, copy the Medium editor contents
-      // to the related textarea.
-      $('form').submit(function(event) {
-        $('.editable').each(function() {
-          var editorKey = $field.val().parent().attr('data-editor-key');
-          var editorValue = Drupal.editors.medium[editorKey].serialize();
-          alert( "$field.val()).val()" );
-          $field.val().val(editorValue["element-0"].value);
-        });
-      });
 })(jQuery, Drupal, MediumEditor);
